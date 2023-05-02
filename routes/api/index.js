220185395e0589ab3fbae9ada7d4decde0f803e9
CarlyGallagher <jagllaghercarly3@yahoo.com>
2023-05-01 22:32:04 -0700

    fixing bugs

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,15 @@
+// Set requirements (Express Router)
 const router = require('express').Router();
-const userRoutes = require('./user-routes');
-const thoughtRoutes = require('./thought-routes');
 
-router.use('/users', userRoutes);
-router.use('/thoughts', thoughtRoutes);
+// Set routes (user and thought routes)
+const usersRoutes = require('./user-routes');
+const thoughtsRoutes = require('./thought-routes');
 
+// Add `/users` to created routes 
+router.use('/users', usersRoutes);
+
+// Add `/thoughts` to created routes 
+router.use('/thoughts', thoughtsRoutes);
+
+// Export Module Router
 module.exports = router;
